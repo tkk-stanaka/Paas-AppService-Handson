8ffe546f493c0ec8ef9729939a432226dc7dbaf8
h-morozumi <morozumi.h@outlook.com>
2024-11-26 23:47:30 +0900

    Add PowerShell scripts for installing WinGet and essential packages on Windows

---
+++ template/windows/winget.ps1
@@ -0,0 +1,6 @@
+winget install -e --id Microsoft.WindowsTerminal
+winget install -e --id Microsoft.OpenJDK.17
+winget install -e --id Microsoft.AzureCLI
+winget install -e --id Microsoft.VisualStudioCode
+
+# https://www.oracle.com/jp/database/sqldeveloper/technologies/download/
